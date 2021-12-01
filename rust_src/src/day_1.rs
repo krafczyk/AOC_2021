4eb41b99edc57c8c4212bbbb0ed1b5050a7875d7
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-12-01 16:21:33 -0600

    Solve day 1 part 2 in rust.

--- rust_src/src/day_1.rs
+++ rust_src/src/day_1.rs
@@ -1,8 +1,11 @@
 extern crate argparse;
+extern crate num;
 
 use std::io::{self, BufRead};
 use std::fs::File;
 use std::path::Path;
+use std::iter::Sum;
+use num::Zero;
 use argparse::{ArgumentParser, StoreTrue, Store};
 
 // Define readlines function discussed here:
@@ -11,6 +14,26 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn window_increases<'a, T>(numbers: &'a Vec<T>, window_size: usize) -> usize
+where
+    T: Ord+Sum<&'a T>+Zero
+{
+    let mut is_first: bool = true;
+    let mut last_sum: T = T::zero();
+    let mut num_increases: usize = 0;
+    for i in 0..(numbers.len()-(window_size-1)) {
+        let sum = numbers[i..i+window_size].iter().sum();
+        if !is_first {
+            if sum > last_sum {
+                num_increases += 1;
+            }
+        }
+        is_first = false;
+        last_sum = sum;
+    }
+    return num_increases;
 }
 
 fn main() {
@@ -30,23 +53,18 @@ fn main() {
     }
 
     // Reading the file
-    let mut is_first: bool = true;
-    let mut last_num: u32 = 0;
-    let mut num_increases = 0;
+    let mut numbers = Vec::new();
     if let Ok(lines) = read_lines(input) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
                 let num: u32 = ip.trim().parse().expect("Not a number");
-                if !is_first {
-                    if num > last_num {
-                        num_increases += 1;
-                    }
-                }
-                is_first = false;
-                last_num = num;
+                numbers.push(num);
             }
         }
     }
-    println!("Day 1: {} increases", num_increases)
+
+
+    println!("Day 1: {} increases", window_increases(&numbers, 1));
+    println!("Day 2: {} increases", window_increases(&numbers, 3));
 }
